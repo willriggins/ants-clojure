4058539dc4f8b3de6a999bf37fc9b1620f41c58b
willriggins <williamriggins2013@gmail.com>
2016-07-14 12:12:38 -0400

    ants movement and fps counter

--- src/ants_clojure/core.clj
+++ src/ants_clojure/core.clj
@@ -4,13 +4,56 @@
 
 (def width 800)
 (def height 600)
+(def ant-count 100)
+(def ants(atom []))
+
+(defn create-ants []
+  (for [i (range 0 ant-count)]
+    {:x (rand-int width)
+     :y (rand-int height)}))
+
+(defn draw-ants! [context]
+  (.clearRect context 0 0 width height) 
+  (doseq [ant @ants]
+    (.setFill context javafx.scene.paint.Color/BLACK)
+    (.fillOval context (:x ant) (:y ant) 5 5)))
+
+(defn move-ant [ant]
+  (Thread/sleep 1)
+  (let [x-change (- (* 2 (rand)) 1)
+        y-change (- (* 2 (rand)) 1)]
+    (assoc ant 
+      :x (+ (:x ant) x-change)
+      :y (+ (:y ant) y-change))))
+
+(defn move-ants []
+  (pmap move-ant @ants))
+
+(def last-timestamp (atom 0))
+
+(defn fps [current-timestamp]
+  (let [diff (- current-timestamp @last-timestamp)
+        diff-seconds (/ diff 1000000000)]
+    (int (/ 1 diff-seconds))))
+  
 
 (defn -start [app stage]
   (let [root (javafx.fxml.FXMLLoader/load (io/resource "main.fxml"))
-        scene (javafx.scene.Scene. root width height)]
+        scene (javafx.scene.Scene. root width height)
+        canvas (.lookup scene "#canvas")
+        context (.getGraphicsContext2D canvas)
+        fps-label (.lookup scene "#fps")
+        timer (proxy [javafx.animation.AnimationTimer] []
+                (handle [now]
+                  (reset! ants (move-ants))
+                  (draw-ants! context)
+                  (.setText fps-label (str (fps now)))
+                  (reset! last-timestamp now)))]
     (.setTitle stage "Ants")
     (.setScene stage scene)
-    (.show stage)))
+    (.show stage)
+    (reset! ants (create-ants))
+    (.start timer)))
 
 (defn -main []
   (javafx.application.Application/launch ants_clojure.core (into-array String [])))
